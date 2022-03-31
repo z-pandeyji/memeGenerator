30e2b248aca910fc92e9007b5503283efd06964c
Shivanshu Pandey <shivanshup1@gmail.com>
2022-04-01 01:49:44 +0530

    'second-commit'

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,16 @@
 import React from'react';
 
 export default function Header(){
-    return(
-        <header>
-            <img className='header-image' src="/images/face-meme-png.png"/>
-            <h2 className='header-title'>Meme Generator</h2>
-            <h4 className='header-connect'>connect with me on: <a href="#">LinkedIn</a></h4>
-        </header>
-    )
+    return (
+      <header>
+        <img className="header-image" src="/images/face-meme-png.png" />
+        <h2 className="header-title">Meme Generator</h2>
+        <h4 className="header-connect">
+          connect with me on:{' '}
+          <a href="https://www.linkedin.com/in/shivanshu-pandey-ji/" target="_blank">
+            LinkedIn
+          </a>
+        </h4>
+      </header>
+    );
 }
